ce403db3806355a6b15e651aeb324f0f47d70014
KHALIFA Haithem <khalifa.haithem@gmail.com>
2025-02-01 22:16:29 +0100

    Add client

---
+++ src/SchemaBot/SchemaBot.SettingService.Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchemaBot.SettingService.Client
+{
+    // RegistrationExtensions.cs
+    using Microsoft.Extensions.DependencyInjection;
+    using Polly.Extensions.Http;
+    using Polly;
+    using Refit;
+    using System.Net.Http.Headers;
+    using System.Text.Json.Serialization;
+    using System.Text.Json;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSchemaBotClient(
+            this IServiceCollection services,
+            string baseAddress)
+        {
+            services.AddRefitClient<ISchemaBotApi>(new RefitSettings
+            {
+                ContentSerializer = new SystemTextJsonContentSerializer(
+                        new JsonSerializerOptions
+                        {
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                            Converters = { new JsonStringEnumConverter() }
+                        })
+            })
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(baseAddress))
+                .AddHttpMessageHandler<AuthHeaderHandler>()
+                .AddPolicyHandler(GetRetryPolicy());
+
+            return services;
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(3, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+    }
+
+    // AuthHeaderHandler.cs
+    public class AuthHeaderHandler : DelegatingHandler
+    {
+        private readonly IAuthTokenProvider _tokenProvider;
+
+        public AuthHeaderHandler(IAuthTokenProvider tokenProvider)
+        {
+            _tokenProvider = tokenProvider;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var token = await _tokenProvider.GetTokenAsync();
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+
+    // IAuthTokenProvider.cs
+    public interface IAuthTokenProvider
+    {
+        Task<string?> GetTokenAsync();
+        Task SetTokenAsync(string token);
+    }
+
+    // MemoryTokenProvider.cs
+    public class MemoryTokenProvider : IAuthTokenProvider
+    {
+        private string? _token;
+
+        public Task<string?> GetTokenAsync() => Task.FromResult(_token);
+
+        public Task SetTokenAsync(string token)
+        {
+            _token = token;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/SchemaBot/SchemaBot/SchemaBot.Client/Services/SchemaBotService.cs
@@ -0,0 +1,38 @@
+﻿using SchemaBot.SettingService.Client;
+
+namespace SchemaBot.Client.Services
+{
+    public class SchemaBotService
+    {
+        private readonly ISchemaBotApi _api;
+        private readonly IAuthTokenProvider _tokenProvider;
+
+        public SchemaBotService(
+            ISchemaBotApi api,
+            IAuthTokenProvider tokenProvider)
+        {
+            _api = api;
+            _tokenProvider = tokenProvider;
+        }
+
+        public async Task InitializeAsync(string username, string password)
+        {
+            var loginResponse = await _api.LoginAsync(new LoginRequest(
+                Username: username,
+                Password: password));
+
+            if (loginResponse.IsSuccessStatusCode)
+            {
+                await _tokenProvider.SetTokenAsync(loginResponse.Content.Token);
+            }
+        }
+
+        public async Task<ApiConfiguration> UpdateConfigAsync(
+            Guid configId,
+            ApiConfiguration updatedConfig)
+        {
+            var response = await _api.UpdateConfigurationAsync(configId, updatedConfig);
+            return response.Content;
+        }
+    }
+}
